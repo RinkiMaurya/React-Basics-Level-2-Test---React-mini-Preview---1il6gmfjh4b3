0878c3a0fdb3aae6d78f7995950e91f05013bbdf
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-04-18 16:20:33 +0530

    Input

--- src/components/App.js
+++ src/components/App.js
@@ -2,44 +2,44 @@ import React, { useState } from "react";
 import "../styles/App.css";
 import { Preview } from "./Preview";
 const App = () => {
-  const [content, setContent] = useState("hello world");
-  const [padding, setPadding] = useState(5);
-  const [fontSize, setFontSize] = useState(10);
+  const [state1, setState1] = useState("hello world");
+  const [state2, setState2] = useState(5);
+  const [state3, setState3] = useState(10);
   return (
     <div id="main">
       <div>
-        <label>Contant </label>
+        {/* <label>Contant </label> */}
         <input
           type="text"
           id="contentInput"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
+          value={state1}
+          onChange={(e) => setState1(e.target.value)}
         />
-        <br />
-        <label>padding </label>
+        {/* <br /> */}
+        {/* <label>padding </label> */}
         <input
           type="number"
           id="paddingInput"
-          value={padding}
-          onChange={(e) => setPadding(e.target.value)}
+          value={state2}
+          onChange={(e) => setState2(e.target.value)}
         />
-        <br />
-        <label>fontSize </label>
+        {/* <br />
+        <label>fontSize </label> */}
         <input
           type="number"
           id="fontSizeInput"
-          value={fontSize}
-          onChange={(e) => setFontSize(e.target.value)}
+          value={state3}
+          onChange={(e) => setState3(e.target.value)}
         />
-        <br />
-        <br />
+        {/* <br />
+        <br /> */}
 
         {/* add input elememnts inside here */}
       </div>
       <Preview
-        padding={padding + "px"}
-        fontSize={fontSize + "px"}
-        content={content}
+        padding={state2 + "px"}
+        fontSize={state3 + "px"}
+        content={state1}
       />
       {/* render Preview component here */}
     </div>
