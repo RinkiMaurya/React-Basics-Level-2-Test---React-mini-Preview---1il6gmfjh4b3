20dca853319befbc4f9eda2eafad4ce002cb9fb8
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-04-18 16:50:41 +0530

    input

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,36 @@ import React, { useState } from "react";
 import "../styles/App.css";
 import { Preview } from "./Preview";
 const App = () => {
+  const [state1, setState1] = useState("hello world");
+  const [state2, setState2] = useState(10);
+  const [state3, setState3] = useState(5);
   return (
     <div id="main">
-      <div></div>
-      <Preview />
+      <div>
+        <input
+          type="text"
+          id="contentInput"
+          value={state1}
+          onChange={(e) => setState1(e.target.value)}
+        />
+        <input
+          type="text"
+          id="fontSizeInput"
+          value={state2}
+          onChange={(e) => setState2(e.target.value)}
+        />
+        <input
+          type="text"
+          id="paddingInput"
+          value={state3}
+          onChange={(e) => setState3(e.target.value)}
+        />
+      </div>
+      <Preview
+        padding={state3 + "px"}
+        fontSize={state2 + "px"}
+        content={state1}
+      />
     </div>
   );
 };
