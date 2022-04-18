ef08637a94d042490812e36fff1db706623a75cd
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-04-18 16:26:27 +0530

    hii

--- src/components/App.js
+++ src/components/App.js
@@ -3,42 +3,33 @@ import "../styles/App.css";
 import { Preview } from "./Preview";
 const App = () => {
   const [state1, setState1] = useState("hello world");
-  const [state2, setState2] = useState(5);
-  const [state3, setState3] = useState(10);
+  const [state2, setState2] = useState(10);
+  const [state3, setState3] = useState(5);
   return (
     <div id="main">
       <div>
-        {/* <label>Contant </label> */}
         <input
           type="text"
           id="contentInput"
           value={state1}
           onChange={(e) => setState1(e.target.value)}
         />
-        {/* <br /> */}
-        {/* <label>padding </label> */}
-        <input
-          type="number"
-          id="paddingInput"
-          value={state2}
-          onChange={(e) => setState2(e.target.value)}
-        />
-        {/* <br />
-        <label>fontSize </label> */}
         <input
           type="number"
           id="fontSizeInput"
+          value={state2}
+          onChange={(e) => setState2(e.target.value)}
+        />
+        <input
+          type="number"
+          id="paddingInput"
           value={state3}
           onChange={(e) => setState3(e.target.value)}
         />
-        {/* <br />
-        <br /> */}
-
-        {/* add input elememnts inside here */}
       </div>
       <Preview
-        padding={state2 + "px"}
-        fontSize={state3 + "px"}
+        padding={state3 + "px"}
+        fontSize={state2 + "px"}
         content={state1}
       />
       {/* render Preview component here */}
